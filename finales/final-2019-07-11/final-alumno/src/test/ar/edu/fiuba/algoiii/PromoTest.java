c84c9d20766478b9dfda6c1f75cba4256feef93d
Valen <61921501+valencorrea@users.noreply.github.com>
2020-09-13 22:55:24 -0300

    refactor clases de tests

--- finales/final-2019-07-11/final-alumno/src/test/ar/edu/fiuba/algoiii/PromoTest.java
+++ finales/final-2019-07-11/final-alumno/src/test/ar/edu/fiuba/algoiii/PromoTest.java
@@ -1,36 +1,15 @@
 package test.ar.edu.fiuba.algoiii;
 
-import main.ar.edu.fiuba.algoiii.*;
+import main.ar.edu.fiuba.algoiii.DosPorUno;
+import main.ar.edu.fiuba.algoiii.Gaseosa;
+import main.ar.edu.fiuba.algoiii.Precio;
 import main.ar.edu.fiuba.algoiii.monedas.Dolares;
 import main.ar.edu.fiuba.algoiii.monedas.Pesos;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class PitufosTest {
-
-    @Test
-    public void testUnaGaseosaConCosto100YPrecio200PesosSale100DeRenta(){
-
-        Gaseosa gaseosa = new Gaseosa(100);
-        gaseosa.asociarPrecioIndivudial(new Precio(200, new Pesos()));
-
-        double renta = gaseosa.renta();
-
-        assertEquals(renta, 100.0);
-    }
-
-    @Test
-    public void testUnaGaseosaConCosto100YPrecio10DolaresSale400DeRenta(){
-
-        Gaseosa gaseosa = new Gaseosa(100);
-        gaseosa.asociarPrecioIndivudial(new Precio(10, new Dolares()));
-
-        double renta = gaseosa.renta();
-
-        assertEquals(400.0, renta);
-
-    }
+public class PromoTest {
 
     @Test
     public void testDosGaseosasConCosto100YPrecio1200EnPesosTienePromo2X1(){
