1505ed79fd6f4d665ec3369252c7b31e30d2e016
Valen <61921501+valencorrea@users.noreply.github.com>
2020-09-14 06:25:25 -0300

    modelo final post refactors

--- finales/final-2019-02-28/final-aves-alumno/src/tests/AvestruzTest.java
+++ finales/final-2019-02-28/final-aves-alumno/src/tests/AvestruzTest.java
@@ -31,31 +31,16 @@ public class AvestruzTest {
         assertEquals(velocidad, 0);
 
     }
-/*
+
     @Test
-    public void testUnAvestruzEnUnaPistaDeHieloCorreA70KmPorHora(){
+    public void testUnAvestruzEnUnaSuperficieDeBarroCorreA70KmPorHora(){
 
         Avestruz avestruz = new Avestruz();
 
-        int velocidad = avestruz.correr(new PistaDeHielo());
+        int velocidad = avestruz.correr(new SupDeBarro());
 
         assertEquals(velocidad, 70);
 
-    }*/
-
-
-
-    /*
-    *
-
-		if (superficie == Superficie.HIELO) {
-			// En realidad el hielo si.
-			return 0;
-		}
-
-		return -1;
-	}
-    *
-    * */
+    }
 
 }
