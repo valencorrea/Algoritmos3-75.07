ec4febaf256e5b0305098a1c8706ce1c41d71000
Valen <61921501+valencorrea@users.noreply.github.com>
2020-09-15 23:41:15 -0300

    completo tests de persona sin enfermedades

---
+++ finales/final-2019-07-25/final-alumno2.0/src/test/PersonaCeliacaTest.java
@@ -0,0 +1,23 @@
+package test;
+
+public class PersonaCeliacaTest {
+}
+
+	/*
+	@Test
+	public void personaCeliacaComeCosasParaCeliacosEstaTodoBien() {
+
+		Persona celiaco = new Persona(true);
+		Assert.assertEquals(celiaco.calorias,0);
+
+		PanaderoCeliaco panaderoCeliaco = new PanaderoCeliaco();
+		String pan = panaderoCeliaco.hacerPan();
+		celiaco.comer(pan);
+		Assert.assertEquals(celiaco.calorias,10);
+
+		EmpanaderoCeliaco empanderoCeliaco = new EmpanaderoCeliaco();
+		String empanada = empanderoCeliaco.hacerEmpanada();
+		celiaco.comer(empanada);
+		Assert.assertEquals(celiaco.calorias,25);
+	}
+*/
